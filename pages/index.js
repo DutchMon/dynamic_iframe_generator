d7e788147ecf25268ea7b861597d6065970ece0f
DutchMon <dutch.holland13@gmail.com>
2022-05-10 11:41:46 -0700

    added title header

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
         <meta name="description" content="Easier way to watch stuff" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
+        <div className={styles.title}></div>
         <div className="iframe-container videoContainer">
           <iframe loading="lazy" src="https://xestreams.com/livetv/tv11.php"  allow="fullscreen" sandbox="allow-scripts allow-same-origin allow-presentation"></iframe>
         </div>
