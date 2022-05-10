d59b61c2f3dccd6debf17511169af007ca2f069b
DutchMon <dutch.holland13@gmail.com>
2022-05-10 13:56:17 -0700

    changed visual aspects and url

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,33 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Watch Stuff</title>
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <meta name="description" content="Easier way to watch stuff" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.title}>Hello</div>
-        <div className="iframe-container videoContainer">
-          <iframe loading="lazy" src="http://xestreams.com/livetv/tv11.php"  allow="fullscreen" sandbox="allow-scripts allow-same-origin allow-presentation"></iframe>
+        <div className="column-container">
+          <div className="left-column"></div>
+          <div className="main-column">
+            <div className={styles.title}>Enjoy the game</div>
+          </div>
+          <div className="right-column"></div>
+        </div>
+        <div className="column-container">
+          <div className="left-column"></div>
+          <div className="main-column">
+            <div className="videoContainer">
+              <div className="iframe-container">
+                <iframe loading="lazy" src="http://weakstreams.com/streams/10236821" allow="fullscreen" sandbox="allow-scripts allow-same-origin allow-presentation"></iframe>
+              </div>
+            </div>
+          </div>
+          <div className="right-column"></div>
         </div>
       </main>
     </div>
