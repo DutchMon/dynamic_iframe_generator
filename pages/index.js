a2dca18595db4b09f7b1627fddcebd73fce174f6
DutchMon <dutch.holland13@gmail.com>
2022-05-10 13:05:47 -0700

    changed iframe src back to http

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.title}>Hello</div>
         <div className="iframe-container videoContainer">
-          <iframe loading="lazy" src="https://xestreams.com/livetv/tv11.php"  allow="fullscreen" sandbox="allow-scripts allow-same-origin allow-presentation"></iframe>
+          <iframe loading="lazy" src="http://xestreams.com/livetv/tv11.php"  allow="fullscreen" sandbox="allow-scripts allow-same-origin allow-presentation"></iframe>
         </div>
       </main>
     </div>
